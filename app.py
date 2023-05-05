ebf5ebdcea8e98b2a3bc7ca9fecf4a5f12971a9a
Alex Raduca <alexandru.raduca@gmail.com>
2023-05-05 17:58:35 +0300

    update

--- app.py
+++ app.py
@@ -15,18 +15,18 @@ for doc in clients_ref.stream():
 
 # @st.cache_data
 
-
-def on_snapshot(_doc_snapshot, changes, read_time):
-    for doc in _doc_snapshot:
-        client_data = doc.to_dict()
-        row = [
-            client_data['name'],
-            # client_data['inviteEmails'],
-        ]
-        data.append(row)
-        print('row', row)
-
-
 st.text('Clients')
-st.table(data)
+
+
+def on_snapshot(doc_snapshot, changes, read_time):
+    for doc in doc_snapshot:
+        client_data = doc.to_dict()
+        # row = [
+        #     client_data['name'],
+        #     # client_data['inviteEmails'],
+        # ]
+        st.json(client_data)
+        # print('row', row)
+
+
 collection_watch = clients_ref.on_snapshot(on_snapshot)
